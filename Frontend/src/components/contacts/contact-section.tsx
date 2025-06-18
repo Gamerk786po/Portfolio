4a4a172c922724d7454982f91c7177a2d57781bc
Gamerk786po <abdullahpper@gmail.com>
2025-06-19 02:26:20 +0500

    changes

---
+++ Frontend/src/components/contacts/contact-section.tsx
@@ -0,0 +1,16 @@
+// component for contact-section
+import { motion } from "motion/react"; // Motion for animations
+import { TypeAnimation } from "react-type-animation"; // for typing animations
+// The component
+const ContactSection = () => {
+    // Returning statement
+    return(
+        <>
+        {/* The section for the content of contacts */}
+            <motion.section>
+
+            </motion.section>
+        </>
+    )
+}
+export default ContactSection;
