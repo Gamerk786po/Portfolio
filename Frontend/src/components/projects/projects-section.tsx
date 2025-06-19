2937cf1076f02a8bdbbaf3b5d88bdcea341cb88b
Gamerk786po <abdullahpper@gmail.com>
2025-06-19 05:06:39 +0500

    Changes

--- Frontend/src/components/projects/projects-section.tsx
+++ Frontend/src/components/projects/projects-section.tsx
@@ -25,7 +25,7 @@ const ProjectsSection = () => {
       >
         {/* Heading of showcase */}
         <h1
-          id="projects-heading"
+          id="showcase-heading"
           className="text-white font-bold text-2xl md:text-4xl 2xl:text-5xl leading-relaxed"
         >
           Showcase
